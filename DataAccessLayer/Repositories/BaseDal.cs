059a79f50bea1eb1caef37b77ec5c7ee3b8c400f
dogkansarac <dogkansarac@outlook.com>
2023-07-18 14:07:10 +0300

    test commit

--- DataAccessLayer/Repositories/BaseDal.cs
+++ DataAccessLayer/Repositories/BaseDal.cs
@@ -1,9 +1,6 @@
 ﻿using DataAccessLayer.Concrete;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
