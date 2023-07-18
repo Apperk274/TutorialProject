a310f75a679fefbbe490e9370cc64ddb73b22161
Doğkan Saraç <dogkan.sarac@securifyidentity.com>
2023-07-18 14:04:06 +0300

    feat: Add AuthController

---
+++ TutorialProject/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutorialProject.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
