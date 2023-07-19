f100de1ac6ab9a4408580240f7509c99d327db4c
dogkansarac <dogkansarac@outlook.com>
2023-07-19 11:37:13 +0300

    feat: Add AuthService

---
+++ BusinessLayer/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Repositories;
+using DTOLayer.ReqDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class AuthService
+    {
+        private readonly UserDal _userDal;
+        public AuthService(UserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void Register(RegisterReqDTO registerReq)
+        {
+
+        }
+    }
+}
--- TutorialProject/Controllers/AuthController.cs
+++ TutorialProject/Controllers/AuthController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer;
+using DTOLayer.ReqDTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TutorialProject.Controllers
 {
     public class AuthController : Controller
     {
-
+        private readonly AuthService _authService;
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
         [HttpGet]
         public IActionResult Register()
         {
@@ -13,6 +19,13 @@ namespace TutorialProject.Controllers
         [HttpGet]
         public IActionResult LogIn()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterReqDTO registerReqDTO)
+        {
+            _authService.Register(registerReqDTO);
             return View();
         }
     }
