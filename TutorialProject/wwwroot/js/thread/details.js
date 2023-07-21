0e19907ae800c971315ea42cca6be26310c25cec
Apperk274 <ahberk.tas@gmail.com>
2023-07-21 11:19:53 +0300

    refactor & feat: Isolate js from html and add basic comment elements

---
+++ TutorialProject/wwwroot/js/thread/details.js
@@ -0,0 +1,80 @@
+﻿let elInjector
+
+let elShowCommentsButton
+let elCommentsContainer
+let templateComment
+
+function showComments() {
+    hideEl(elShowCommentsButton)
+    $.ajax({
+        type: "GET",
+        url: inject("comments-request-url"),
+        dataType: "json",
+        success: function (comments) {
+            for (comment of comments) {
+                const commentEl = createCommentEl()
+                elCommentsContainer.appendChild(commentEl)
+            }
+            switchCaretIcon()
+            elShowCommentsButton.setAttribute("onclick", "hideComments()")
+        },
+        error: function (req, status, error) {
+            console.log(msg);
+        },
+        complete: function () {
+            showEl(elShowCommentsButton)
+        }
+    });
+}
+
+function hideComments() {
+    elCommentsContainer.innerHTML = ""
+    elShowCommentsButton.setAttribute("onclick", "showComments()")
+    switchCaretIcon()
+}
+
+// Util functions
+
+function createCommentEl() {
+    const commentEl = templateComment.content.cloneNode(true)
+    commentEl.querySelector("#title").innerText = comment.title
+    commentEl.querySelector("#content").innerText = comment.content
+    commentEl.querySelector("#user").innerText = comment.appUserId
+    commentEl.querySelector("#likeCount").innerText = comment.appUserId
+    commentEl.querySelector("#dislikeCount").innerText = comment.appUserId
+    return commentEl
+}
+
+function switchCaretIcon() {
+    const classUp = "bi-caret-up-square"
+    const classDown = "bi-caret-down-square"
+    if (elShowCommentsButton.classList.contains(classDown)) {
+        elShowCommentsButton.classList.remove(classDown)
+        elShowCommentsButton.classList.add(classUp)
+    }
+    else {
+        elShowCommentsButton.classList.remove(classUp)
+        elShowCommentsButton.classList.add(classDown)
+    }
+}
+
+// General functions
+
+function showEl(el) {
+    el.classList.remove("d-none")
+}
+function hideEl(el) {
+    el.classList.add("d-none")
+}
+function inject(name) {
+    return elInjector.getAttribute("data-" + name)
+}
+
+// Referencing elements
+
+window.onload = function () {
+    elShowCommentsButton = document.querySelector("#showCommentsButton")
+    templateComment = document.querySelector("template#comment")
+    elCommentsContainer = document.querySelector("#commentsContainer")
+    elInjector = document.querySelector("#injector")
+}
