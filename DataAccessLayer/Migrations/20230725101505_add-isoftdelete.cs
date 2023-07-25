4218bf4130e36c08ce1052f7381650d26cdacb8e
dogkansarac <dogkansarac@outlook.com>
2023-07-25 13:15:38 +0300

    feat: Add ISoftDelete migration

---
+++ DataAccessLayer/Migrations/20230725101505_add-isoftdelete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class addisoftdelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Threads",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Threads");
+        }
+    }
+}
